001b8d84bad29a3e296e6c3a139ca2e5a1ae24a3
evgesha <zplacesound@gmail.com>
2020-03-12 06:08:41 +0300

    with qt changes

---
+++ testmain.cpp
@@ -0,0 +1,19 @@
+#include <QApplication>
+#include <QLabel>
+#include <QWidget>
+#include "ncompress.cpp"
+
+
+
+int main(int argc, char *argv[ ])
+{
+Ncompress d;
+d.compress();
+QApplication app(argc, argv);
+QLabel hello("<center>Welcome to my first Qt program</center>");
+hello.setWindowTitle("My First Qt Program");
+hello.resize(400, 400);
+hello.show();
+return app.exec();
+
+}
