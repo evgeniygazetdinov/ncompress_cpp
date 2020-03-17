8f02528bd209aa6d31a09099fe85a9c1bda77e61
evgeniy gazetdinov <zplacesound@gmail.com>
2020-03-17 11:46:38 +0300

    replace serializer (#7)

--- ncompress.cpp
+++ ncompress.cpp
@@ -6,92 +6,71 @@
 #include <QDebug>
 #include <QStringList>
 #include <QString>
-#include <QStringList>
-
+#include <QVector>
+#include <QFile>
 #include <QDataStream>
 
-
-
-
+QByteArray Ncompress::serialize(QVector<int> vec)
+{
+    QByteArray byteArray;
+    QDataStream stream(&byteArray, QIODevice::WriteOnly);
+    stream << vec;
+    QFile file("1.bin");
+    if(file.open(QIODevice::WriteOnly))
+    {
+    QDataStream stream(&file);
+    stream << vec;
+    file.close();
+    }
+    return byteArray;
+};
 
 QByteArray Ncompress::compress(const QByteArray  for_compress)
 {   
-    //try with another one place for save n
-    QByteArray res;
-    vector<int> res2;
-    
-
-    int dictSize = 256;
-    std::map<std::string,int> dict;
-    for (int i = 0; i < 256; i++)
-    {
-        dict[std::string(1, i)] = i;
-    } 
-    std::string w;
-    //convert qbyte to string
-
-    std::string ar (for_compress.constData(), for_compress.length());
-    for (string::const_iterator it = ar.begin();
-      it != ar.end(); ++it) {
-    char c = *it;
-    std::string wc = w+ c;
-    
-    if (dict.count(wc))
-      {
-        w = wc;
-      }
-    else {
-      //right one
-      res.append(dict[w]);
-      res2.push_back(dict[w]);
-      dict[wc] = dictSize++;
-      
-      w = std::string(1, c);
-    }
-  }
-
-  if (!w.empty())
-    {res.append(dict[w]);
-    res2.push_back(dict[w]);
-    }
-  	
-    for(int i=0;i<res.size();i++){ 
-		std::cout << res2[i]<<endl;
-    }
-  return res;
-};
-QByteArray Ncompress::decompress(const QByteArray& for_decompress)
-{  /*
+  QVector<int> res2;
   QByteArray res;
-  std::string word_for_decompress (for_decompress.data(), for_decompress.length());
-  int end = word_for_decompress.end();
-  int begin = word_for_decompress.begin();
-  // Build the dictionary.
   int dictSize = 256;
-  std::map<int,std::string> dict;
+  std::map<std::string,int> dict;
   for (int i = 0; i < 256; i++)
   {
-    dict[i] = std::string(1, i);
+      dict[std::string(1, i)] = i;
+  } 
+  std::string w;
+  //convert qbyte to string
+  std::string ar (for_compress.constData(), for_compress.length());
+  for (string::const_iterator it = ar.begin();
+    it != ar.end(); ++it) {
+  char c = *it;
+  std::string wc = w+ c;
+  
+  if (dict.count(wc))
+    {
+      w = wc;
+
+    }
+  else {
+    //right one
+    res2.push_back(dict[w]);
+    dict[wc] = dictSize++;
+    
+    w = std::string(1, c);
   }
-  //std::string w(1, *begin++);
-  //std::string result = w;
-  std::string entry;
- 
-  for ( ; begin != end; begin++) {
-    int k = *begin;
-    if (dict.count(k))
-      entry = dict[k];
-    else if (k == dictSize)
-      entry = w + w[0];
-    else
-      throw "Bad compressed k";
- 
-    result += entry;
- 
-    // Add w+entry[0] to the dictionary.
-    dict[dictSize++] = w + entry[0];
- 
-    w = entry;
-  }*/
+}
+
+  if (!w.empty())
+    {
+    res2.push_back(dict[w]);
+    }
+
+    for(int i=0;i<res2.size();i++){ 
+    std::cout << res2[i]<<endl;
+    }
+
+  res = (this->serialize(res2));
+return res;
+};
+
+QByteArray Ncompress::decompress(const QByteArray& for_decompress)
+{ 
   return for_decompress;
 };
